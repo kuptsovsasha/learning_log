f70af63fc53d544ee1342f540270f1dc28a52a29
Kuptsov <kuptsovsasha@gmail.com>
2021-10-26 06:51:32 +0300

    Add bootstrap elemrnts for pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,14 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #my apps
     'users',
+    'learning_logs',
+
+    #third party apps
+    'bootstrap4',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'learning_logs',
 
 ]
 
